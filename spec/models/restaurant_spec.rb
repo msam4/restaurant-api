968f84c9498a8ecb08620400beb6f28224e2bda6
Michael Sam <sam.michael.h@gmail.com>
2024-01-13 11:56:08 +0900

    Fix the warden issue for restaurant controller. Fix uniqueness rspec test for restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
   let(:user) { User.create(email: 'michael@email.com', password: 'testtest') }
-  let(:restaurant) { Restaurant.new(name: 'McDonald', address: 'New York City', user: user) }
+  let(:restaurant) { Restaurant.create(name: 'McDonald', address: 'New York City', user: user) }
 
   describe '#initialize' do
 
@@ -59,16 +59,18 @@ RSpec.describe Restaurant, type: :model do
   ## Unable to get uniqueness of name to work. The false test is always failing.
   ## Add a new migration to add uniqueness to restaurant name's index but the test still fails
 
-  # context 'when checking uniqueness of name' do
-  #   it 'return true if the name is unique' do
-  #     # test code here
-  #     expect(restaurant).to be_valid
-  #   end
+  context 'when checking uniqueness of name' do
+    it 'return true if the name is unique' do
+      # test code here
+      expect(restaurant).to be_valid
+    end
 
-  #   it 'return false if the name is not unique' do
-  #     # test code here
-  #     duplicate_restaurant = Restaurant.new(name: 'McDonald', address: 'Denver', user: user)
-  #     expect(duplicate_restaurant).not_to be_valid
-  #   end
-  # end
+    it 'return false if the name is not unique' do
+      # test code here
+      params = { name: 'Unique', address: 'Tokyo', user: user }
+      Restaurant.create(params)
+      duplicate_restaurant = Restaurant.new(name: 'Unique', address: 'Tokyo', user: user)
+      expect(duplicate_restaurant).not_to be_valid
+    end
+  end
 end
