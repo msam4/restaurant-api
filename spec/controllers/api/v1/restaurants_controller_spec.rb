597551d4da78ae1aea500517b245c487eee2bcb2
Michael Sam <sam.michael.h@gmail.com>
2024-01-13 18:11:59 +0900

    Try to rspec controller create action for restaurant but failed

--- spec/controllers/api/v1/restaurants_controller_spec.rb
+++ spec/controllers/api/v1/restaurants_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::RestaurantsController, type: :controller do
+RSpec.describe API::V1::RestaurantsController, type: :controller do
   describe '#create' do
     it 'should save an instance of restaurant to the db' do
       user = User.create(email: 'sam@email.com', password: 'testtest')
       params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
 
       post :create, params: params
-      expect(Restaurant.last.address).to eq('Boston')
+      expect(Restaurant.last.name).to eq('Wendys')
     end
   end
 end
---
+++ spec/requests/requests_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+include Rails.application.routes.url_helpers
+
+RSpec.describe API::V1::RestaurantsController, type: :request do
+  describe '#create' do
+    it 'should save an instance of restaurant to the db' do
+      user = User.create(email: 'tom@email.com', password: 'testtest')
+      params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
+      auth_token = user.authentication_token
+
+      post "#{root_url}api/v1/restaurants", params: params, headers: { "X-User-Token": auth_token }
+      binding.break
+      expect(Restaurant.last.name).to eq('Wendys')
+    end
+  end
+end
