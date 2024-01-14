bae2eb509e6570e2e7bf1ddd8eb49a37797cc3d9
Michael Sam <sam.michael.h@gmail.com>
2024-01-14 22:32:49 +0900

    Attempt to make update rspec test for restaurant controller but update information is not being saved

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 include Rails.application.routes.url_helpers
 
 RSpec.describe API::V1::RestaurantsController, type: :request do
+  let(:user) { User.create(email: 'tom@email.com', password: 'testtest') }
+  let(:params) { { restaurant: { name: 'Wendys', address: 'Boston', user: user } } }
+  let(:user_email) { user.email }
+  let(:auth_token) { user.authentication_token }
+
   describe '#create' do
-    let(:user) { User.create(email: 'tom@email.com', password: 'testtest') }
-    let(:params) { { restaurant: { name: 'Wendys', address: 'Boston', user: user } } }
-    let(:user_email) { user.email }
-    let(:auth_token) { user.authentication_token }
     before { post "#{root_url}api/v1/restaurants", params: params, headers: { "X-User-Email": user_email, "X-User-Token": auth_token } }
 
     it 'should save an instance of restaurant to the db' do
@@ -21,6 +22,19 @@ RSpec.describe API::V1::RestaurantsController, type: :request do
 
     it 'renders the show' do
       expect(response).to have_http_status(201)
+    end
+  end
+
+  describe '#update' do
+    it 'should update the attributes of a restaurant' do
+      restaurant = Restaurant.create(name: "White Castle", address: "St. Louis", user: user)
+      new_name = "Sonic"
+      new_address = "Chicago"
+      new_params = { restaurant: { name: new_name, address: new_address, user: user } }
+
+      put "#{root_url}api/v1/restaurants/#{restaurant.id}", params: new_params, headers: { "X-User-Email": user_email, "X-User-Token": auth_token }
+      # binding.break
+      expect(restaurant.name).to eq("Sonic")
     end
   end
 end
