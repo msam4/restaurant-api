8c8d135919de77537f079d9794c6c9a2a2ac4356
Michael Sam <sam.michael.h@gmail.com>
2024-01-14 22:09:42 +0900

    DRY create rspec test in the request test. comment out the original code as a reference

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -3,15 +3,24 @@ include Rails.application.routes.url_helpers
 
 RSpec.describe API::V1::RestaurantsController, type: :request do
   describe '#create' do
-    it 'should save an instance of restaurant to the db' do
-      user = User.create(email: 'tom@email.com', password: 'testtest')
-      params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
-      user_email = user.email
-      auth_token = user.authentication_token
+    let(:user) { User.create(email: 'tom@email.com', password: 'testtest') }
+    let(:params) { { restaurant: { name: 'Wendys', address: 'Boston', user: user } } }
+    let(:user_email) { user.email }
+    let(:auth_token) { user.authentication_token }
+    before { post "#{root_url}api/v1/restaurants", params: params, headers: { "X-User-Email": user_email, "X-User-Token": auth_token } }
 
-      post "#{root_url}api/v1/restaurants", params: params, headers: { "X-User-Email": user_email, "X-User-Token": auth_token }
-      # binding.break
+    it 'should save an instance of restaurant to the db' do
+      # user = User.create(email: 'tom@email.com', password: 'testtest')
+      # params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
+      # user_email = user.email
+      # auth_token = user.authentication_token
+
+      # post "#{root_url}api/v1/restaurants", params: params, headers: { "X-User-Email": user_email, "X-User-Token": auth_token }
       expect(Restaurant.last.name).to eq('Wendys')
+    end
+
+    it 'renders the show' do
+      expect(response).to have_http_status(201)
     end
   end
 end
