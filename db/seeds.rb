b4e9721d3b055cac0110c937276261d1aa117d98
Michael Sam <sam.michael.h@gmail.com>
2024-01-15 17:30:08 +0900

    Include seed in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,44 @@
 # --------------------------------------------------
 # Seeding fake restaurants for data purposes
 
-# User.destroy_all
-# Restaurant.destroy_all
+Comment.destroy_all
+Restaurant.destroy_all
+User.destroy_all
 
-# puts 'Destroying all user'
-# puts 'Destroying all restaurants'
+puts 'Destroying all comments'
+puts 'Destroying all user'
+puts 'Destroying all restaurants'
 
 # # Create user
-# user = User.create(email: 'sam@email.com', password: 'testtest')
-# puts 'Created user'
+user1 = User.create(email: "michael@email.com", password: "testtest")
+user2 = User.create(email: 'sam@email.com', password: 'testtest')
+puts 'Created users'
 
-# # Create 10 restaurants
-# puts 'Creating 10 restaurants'
+# Creating 2 restaurants
+puts "Creating 2 restaurants"
+restaurant1 = Restaurant.create(name: "Kogane", address: "Meguro", user: user1)
+restaurant2 = Restaurant.create(name: "Hachigo", address: "Ginza", user: user1)
+puts "Created 2 restaurants"
 
-# 10.times do
-#   Restaurant.create(
-#     name: Faker::Restaurant.name,
-#     address: Faker::Address.full_address,
-#     user: user
-#   )
-# end
+# Create 10 restaurants
+puts 'Creating 10 random restaurants'
 
-# puts 'Created 10 restaurants'
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    user: user1
+  )
+end
+
+puts 'Created 10 restaurants'
+
+# Create comments for restaurant 1
+puts "Creating comments"
+comment1 = Comment.create(user: user2, restaurant_id: restaurant1.id, content: "very good!")
+
+# Create comment for restaurant 2
+comment2 = Comment.create(user: user2, restaurant_id: restaurant2.id, content: "super delicious!")
+
+puts "Created comments"
 # ------------------------------------------------
