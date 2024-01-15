663fe1fd6684e97855aad12dd620aa64539609d9
Michael Sam <sam.michael.h@gmail.com>
2024-01-15 12:08:32 +0900

    Comment out the rspec test in restaurant controller spec file. Using the request spec file to test the restaurant controller

--- spec/controllers/api/v1/restaurants_controller_spec.rb
+++ spec/controllers/api/v1/restaurants_controller_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe API::V1::RestaurantsController, type: :controller do
-  describe '#create' do
-    it 'should save an instance of restaurant to the db' do
-      user = User.create(email: 'sam@email.com', password: 'testtest')
-      params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
+# RSpec.describe API::V1::RestaurantsController, type: :controller do
+#   describe '#create' do
+#     it 'should save an instance of restaurant to the db' do
+#       user = User.create(email: 'sam@email.com', password: 'testtest')
+#       params = { restaurant: { name: 'Wendys', address: 'Boston', user: user}}
 
-      post :create, params: params
-      expect(Restaurant.last.name).to eq('Wendys')
-    end
-  end
-end
+#       post :create, params: params
+#       expect(Restaurant.last.name).to eq('Wendys')
+#     end
+#   end
+# end
